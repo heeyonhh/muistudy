fe81fbe86e3ac58c9bccaf7e76bd408d8bb746f1
heeyonhh <lightram23@gmail.com>
2023-07-19 19:10:28 +0900

    button container grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
+// mui 는 grid를 12등분 함 프롭스 item xs 12등분
+// 컨테이너 프롭스 fixed 반응형 좌우 여백 어느정도 있는 배치
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fixed>
+        <Header></Header>
+        <Grid container>
+          <Grid item xs="2">
+            <Nav></Nav>
+          </Grid>
+          <Grid item xs="10">
+            <Acticle></Acticle>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>welcome</h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav style={{ border: '1px solid gray' }}>
+      <ol>
+        <li>html</li>
+        <li>css</li>
+      </ol>
+    </nav>
+  );
+}
+
+function Acticle() {
+  return (
+    <article style={{ border: '1px solid gray' }}>
+      <h2>Welcome</h2>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      <br />
+      <ButtonGroup>
+        <Button variant="outlined">Create</Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
+    </article>
   );
 }
 
