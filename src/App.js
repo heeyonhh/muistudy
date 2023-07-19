6e8dd7584649b724de3bb23a891e48f800b81840
heeyonhh <lightram23@gmail.com>
2023-07-19 19:23:55 +0900

    dialog onclick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-// mui 는 grid를 12등분 함 프롭스 item xs 12등분
-// 컨테이너 프롭스 fixed 반응형 좌우 여백 어느정도 있는 배치
 function App() {
   return (
     <div className="App">
@@ -34,7 +38,7 @@ function Header() {
 
 function Nav() {
   return (
-    <nav style={{ border: '1px solid gray' }}>
+    <nav>
       <ol>
         <li>html</li>
         <li>css</li>
@@ -44,16 +48,40 @@ function Nav() {
 }
 
 function Acticle() {
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <article style={{ border: '1px solid gray' }}>
+    <article>
       <h2>Welcome</h2>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      Lorem Ipsum is simply dummy text.
+      <br />
       <br />
       <ButtonGroup>
-        <Button variant="outlined">Create</Button>
+        <Button
+          variant="outlined"
+          onClick={()=>{
+            setOpen(true);
+          }}>Create</Button>
         <Button variant="outlined">Update</Button>
       </ButtonGroup>
       <Button variant="outlined">Delete</Button>
+      <Dialog open={open}>
+        <DialogTitle>Create</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Hello Create!!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined">Create</Button>
+          <Button
+            variant="outlined"
+            onClick={()=>{
+              setOpen(false);
+            }}>Cancle</Button>
+        </DialogActions>
+      </Dialog>
     </article>
   );
 }
